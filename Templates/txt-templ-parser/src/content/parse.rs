4ef824b24236ec6ee4ef5deea86ca7d55c5777c1
Deckard <thassilo.schulze@posteo.de>
2023-01-14 15:39:57 +0100

    Moving tests back into the correct modules

--- Templates/txt-templ-parser/src/content/parse.rs
+++ Templates/txt-templ-parser/src/content/parse.rs
@@ -444,3 +444,178 @@ pub enum ParseError {
     LocaleError(#[source] Box<dyn std::error::Error>),    
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::utils::LOGGING;
+    use once_cell::sync::Lazy;
+
+    mod correct {
+        use super::*;
+
+        #[test]
+        fn locales_are_accepted() {
+            let locales = vec!["en_US\nHallo", "fr_FR\n{name}"];
+            helper::test_correct_variants(locale, locales);
+        }
+
+        #[test]
+        fn defaults_are_accepted() {
+            Lazy::force(&LOGGING);
+            let key_defaults = vec![
+                "{name:hallo}",  // `text` default for key
+                "{name:$Me}",  // `constant` default for key
+                "{name:${Someone}}",  // `option` default for key
+                "{name:${Kontake:Müller}}",  // `text` default for `option` default for `key`
+            ];
+            helper::test_correct_variants(key, key_defaults);
+            let opt_defaults = vec![
+                "${Someone:{name}}",  // `key` default for option
+            ];
+            helper::test_correct_variants(option, opt_defaults);
+        }
+
+        #[test]
+        fn keys_are_accepted() {
+            let keys = vec!["{name}", "{NAME}", "{NaMe}", "{n}", "{N}", "{08nsf}"];
+            helper::test_correct_variants(key, keys);
+        }
+
+        #[test]
+        fn idents_are_accepted() {
+            let idents = vec!["hallo", "HALLO", "hAlLO", "h4ll0", "823480", "H4LLO"];
+            helper::test_correct_variants(ident, idents);
+
+            let all_symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            let mut scanner = Scanner::new(&all_symbols);
+            assert!(ident(&mut scanner).is_ok());
+        }
+
+        #[test]
+        fn options_are_accepted() {
+            let options = vec!["${Adressat}", "${addressat}", "${NAME}"];
+            helper::test_correct_variants(option, options);
+        }
+
+        #[test]
+        fn constants_are_accepted() {
+            Lazy::force(&LOGGING);
+            let options = vec!["$MyName", "$myname", "$me13", "$3.141"];
+            helper::test_correct_variants(constant, options);
+        }
+
+        #[test]
+        fn templates_are_accepted() {
+            Lazy::force(&LOGGING);
+            let templates = vec![
+                "{key}$Constant${Option}",
+                "Sehr ${Anrede} {name}\n{nachricht}\n$Mfg\n$Sender",
+                "Sehr geehrte Frau {name}\n{nachricht}\nMit freundlichen Grüßen\nBar",
+                "Hallo Herr {name:${Kontake:Müller}}, ich wollte ...",
+            ];
+            helper::test_correct_variants(template, templates);
+        }
+
+        #[test]
+        fn texts_are_accepted() {
+            let texts = vec![
+                "Sehr geehrter Herr Foo \n\t iblbl", "\nHallo", "h", "\nllsf\n",
+                ")_!_&_)*@#*^+_[]0=082q5-=8';,m;,.<''\"",    
+                "\n \t ",
+            ];
+            helper::test_correct_variants(text, texts);
+        }
+    }
+
+    mod incorrect {
+        use super::*;
+
+        #[test]
+        fn keys_are_rejected() {
+            let cases = vec![
+                ("name", "is missing braces"),
+                ("{name", "is missing right brace"),
+                ("name}", "is missing left brace"),
+                ("{&*(^)}", "contains invalid characters"),
+                ("{ /t\n}", "only contains whitespace charactes"),
+                ("{ /tsf\n}", "contains whitespace charactes"),
+            ];
+            helper::test_incorrect_variants(key, cases);
+        }
+
+        #[test]
+        fn idents_are_rejected() {
+            let cases = vec![
+                (" \n \t", "only contains whitespace characters"),
+                ("*)&%%_)+|", "only contains invalid characters"),
+                ("&*!abc", "starts out with invalid characters"),
+            ];
+            helper::test_incorrect_variants(ident, cases);
+        }
+
+        #[test]
+        fn options_are_rejected() {
+            let cases = vec![
+                ("$name", "is missing the braces"),
+                ("{name}", "is missing the dollar sign"),
+                ("${}", "is missing an identifier"),
+                ("$ {name}", "has a whitespace between the dollar sign and the first brace"),
+            ];
+            helper::test_incorrect_variants(option, cases);
+        }
+
+        #[test]
+        fn constants_are_rejected() {
+            let cases = vec![
+                ("$ name", "has a whitespace between the dollar sign and the ident"),
+                ("${name}", "has braces around it's ident"),
+            ];
+            helper::test_incorrect_variants(constant, cases);
+        }
+
+        #[test]
+        fn texts_are_rejected() {
+            let cases = vec![
+                ("{}\nsf{dsf}$", "contains invalid characters"),
+                ("$$}}{}$", "only contains invalid characters"),
+            ];
+            helper::test_incorrect_variants(text, cases);
+        }
+    }
+
+    mod helper {
+        use super::*;
+
+        pub fn test_correct_variants<T, E>(
+            parse_fn: fn(&mut Scanner) -> Result<T, E>,
+            variants: Vec<&str>,
+        )
+        where
+            T: std::fmt::Debug, E: std::error::Error
+        {
+            for variant in variants {
+                let mut scanner = Scanner::new(&variant);
+                assert!(parse_fn(&mut scanner).is_ok());
+            }
+        }
+
+        pub fn test_incorrect_variants<T, E>(
+            parse_fn: fn(&mut Scanner) -> Result<T, E>,
+            cases: Vec<(&str, &str)>,
+        )
+        where
+            T: std::fmt::Debug, E: std::error::Error
+        {
+            for (variant, case) in cases {
+                let mut scanner = Scanner::new(&variant);
+                assert!(
+                    parse_fn(&mut scanner).is_err(),
+                    "An invalid variant: '{}', which {} was falsely accepted!", 
+                    variant,
+                    case,
+                );            
+            }
+        }
+    }
+}
+
