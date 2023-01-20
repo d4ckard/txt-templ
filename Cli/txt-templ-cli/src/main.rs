3fac0807d522a6d0712c7903a5d04c456dca39dd
Deckard <thassilo.schulze@posteo.de>
2023-01-20 21:19:11 +0100

    Initial CLI commit

---
+++ Cli/txt-templ-cli/src/main.rs
@@ -0,0 +1,127 @@
+use std::{fs::File, path::PathBuf, io::Read, process::Command, env};
+use clap::Parser;
+use txt_templ_compiler::template::Template;
+use txt_templ_compiler::{UserContent, UserContentState};
+use once_cell::sync::Lazy;
+use anyhow::Context;
+use giveup::Giveup;
+
+
+// The default path to the file which contains the configuration
+// for the UserContentState
+const USER_CONTENT_STATE_DEFAULT: &str = "$HOME/.template_content_state.yaml";
+// The environment variable containing the path to another
+// file which contains the configuration for the UserContentState
+// If this environment variable is set, its path will overwrite the default one
+const USER_CONTENT_STATE_FILE_ENV: &str = "TEMPLATE_STATE_FILE";
+// Name of the environment variable which contains the user's default editor name
+const EDITOR: &str = "EDITOR";
+// Name of a default editor assumed to be installed on most systems and
+// usable for most users
+const EDITOR_DEFAULT: &str = "nano";
+
+
+#[derive(Parser, Debug)]
+#[command(about = "Fill out templates")]
+struct Args {
+    /// Set the template source file
+    #[arg(value_name = "FILE")]
+    file: PathBuf,
+}
+
+// Read and parse the given template file
+fn get_template(file_name: &PathBuf) -> anyhow::Result<Template> {    
+    // Read the template
+    let mut file = File::open(file_name)
+        .context("Failed to open the template source file")?;
+    let mut buf = String::new();
+    file.read_to_string(&mut buf)
+        .context("Failed to read the template source file")?;
+
+    log::trace!("Successfully read content of template file:\n{}", &buf);
+
+    // Parse the template
+    let template = Template::parse(&buf).context("Parse error")?;
+    Ok(template)
+}
+
+// Read and parse the UserContentState from a file.
+// This file used is either the default at USER_CONTENT_STATE_DEFAULT
+// or the file set in USER_CONTENT_STATE_FILE_ENV
+fn get_user_content_state() -> anyhow::Result<UserContentState> {
+    // Read the UserContentState file
+    let mut file = match env::var(USER_CONTENT_STATE_FILE_ENV) {
+        Ok(file_name) => {
+            File::open(&file_name)
+                .with_context(|| format!("Failed to open file {} containing the user content state", &file_name))?
+        },
+        Err(_) => {
+            File::open(USER_CONTENT_STATE_DEFAULT)
+                .with_context(|| format!("Failed to open default file {} containing the user content state",
+                    USER_CONTENT_STATE_DEFAULT))?
+        },
+    };
+
+    let mut buf = String::new();
+    file.read_to_string(&mut buf)
+        .context("Failed to read the file contaiing the user content state")?;
+
+    let ucs: UserContentState = serde_yaml::from_str(&buf)?;
+    Ok(ucs)
+}
+
+// Open a temporary YAML file containg all entries to UserContent
+// in the user's default editor to allow the user to set the values.
+// The create an instance of UserState from the temporary file.
+fn get_user_content() -> anyhow::Result<UserContent> {
+    // Create a temporary file
+    let temp_file_path = {
+        let mut path = env::temp_dir();
+        path.push("user-content.yaml");
+        path
+    };
+    File::create(&temp_file_path).context("Failed to create temporary file")?;
+
+    // Open the temp file in the user's preferred editor
+    let editor = env::var(EDITOR).unwrap_or_else(|_| EDITOR_DEFAULT.to_owned());
+    let exit_status = Command::new(&editor)
+        .arg(&temp_file_path)
+        .status()
+        .with_context(|| format!("Something went wrong opening the temporary file in {}", &editor))?;
+    if !exit_status.success() {
+        anyhow::bail!("Editing the user content returned an error")
+    }
+
+    // Read the content of the file the user edited
+    let mut buf = String::new();
+    File::open(&temp_file_path).context("Failed to reopen temporary file")?
+        .read_to_string(&mut buf).context("Failed to read all contents of temporary file")?;
+
+    let uc: UserContent = serde_yaml::from_str(&buf)?;
+    Ok(uc)
+}
+
+
+// TODO: Generate a draft for UserContent which can be filled out by the user
+
+fn main() {
+    Lazy::force(&Lazy::new(|| env_logger::init()));
+
+    let args = Args::parse();
+    log::trace!("Successfully parsed arguments from command line: {:?}", &args);
+
+    let template = get_template(&args.file).giveup("Template Error");
+    log::trace!("Successfully parsed content of template file into a valid template:\n{:?}", &template);
+
+    let ucs = get_user_content_state().giveup("Content State Error");
+    log::trace!("Successfully read and parsed user content state:\n{:?}", &ucs);    
+
+    let uc = get_user_content().giveup("Content Error");
+    log::trace!("Successfully read and parsed user content:\n{:?}", &uc);
+
+    // Fill out the template
+    let result = template.fill_out(uc, ucs).giveup("Fill-Out Error");
+    log::trace!("Successfully filled out tempalte:\n{}", &result);
+
+    print!("{}", result);
+}
